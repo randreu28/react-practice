4ee00bbdcc12919d17ed5552da4e42e4cbc59b3b
randreu28 <randreu28@gmail.com>
2022-11-26 21:45:57 +0100

    Recreates a folder structure similar to the project's

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,48 @@
+import { useState } from "react";
+import Folder from "./component/Folder";
+
+import type { folder } from "./types";
+
 export default function App() {
-  return <div className="text-blue-500">Hello world</div>;
+  const [tree, setTree] = useState<folder>({
+    name: "react-practice",
+    isOpen: true,
+    children: [
+      "node_modules",
+      { name: "public", isOpen: false, children: ["vite.svg"] },
+      {
+        name: "src",
+        isOpen: false,
+        children: [
+          { name: "component", isOpen: false, children: ["Folder.tsx"] },
+          "App.tsx",
+          "index.css",
+          "main.tsx",
+          "types.tsx",
+          "vite-env.d.ts",
+        ],
+      },
+      ".gitignore",
+      "index.html",
+      "package.json",
+      "postcss.config.cjs",
+      "tailwind.config.cjs",
+      "tsconfig.json",
+      "tsconfig.node.json",
+      "vite.config.ts",
+      "yarnk.lock",
+    ],
+  });
+
+  return (
+    <div className="h-screen flex">
+      <div className="m-auto text-3xl">
+        <Folder
+          isOpen={tree.isOpen}
+          name={tree.name}
+          children={tree.children}
+        />
+      </div>
+    </div>
+  );
 }

41925e711c502bb9f0c52d92613ef60daee4cf7a
randreu28 <randreu28@gmail.com>
2022-12-06 18:35:21 +0100

    Creates a horrible-looking UI as placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import { useQuery } from "react-query";
 import getProjects from "./helpers/getProjects";
+import ReactMarkdown from "react-markdown";
 
 export default function App() {
   const { data, error, isLoading } = useQuery("getProjects", getProjects);
 
-  isLoading ? null : console.log(data);
+  if (isLoading) {
+    return (
+      <div className="h-screen flex">
+        <div className="m-auto text-2xl">Loading...</div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="h-screen flex">
+        <div className="m-auto text-red-500 text-2xl">
+          An error occured. Please try again later
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="h-screen flex">
-      <div className="m-auto">Hello world</div>
+      <div className="m-auto">
+        {data?.map((project, key) => {
+          return (
+            <div key={key}>
+              <h1 className="capitalize">
+                {project.name.split("-").join(" ")}
+              </h1>
+              <p>{project.date}</p>
+              <p>{project.url}</p>
+              <ReactMarkdown className="prose prose-a:text-blue-600">
+                {project.description}
+              </ReactMarkdown>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }

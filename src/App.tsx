5922afd74dba720f3a1016fd389acc2cecd972b6
randreu28 <randreu28@gmail.com>
2022-12-06 14:35:40 +0100

    Creates a simple fetch from the github API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+
 export default function App() {
+  type project = {
+    name: string;
+    url: string;
+    date: string;
+  };
+
+  const [projects, setProjects] = useState<project[]>();
+
+  async function fetchRepoInfo() {
+    const rawQuery = await fetch(
+      "https://api.github.com/repos/randreu28/react-practice/branches",
+      {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_GH_API_KEY}`,
+        },
+      }
+    );
+    return await rawQuery.json();
+  }
+
+  useEffect(() => {
+    console.log(fetchRepoInfo());
+  }, []);
+
   return (
     <div className="h-screen flex">
       <div className="m-auto">Hello world</div>

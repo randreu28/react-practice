e6827378c46b65b940dd4cdc9aca9123cabcd002
Rubén Chiquin <randreu28@gmail.com>
2023-03-16 20:23:14 +0100

    Creates Your info layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ export default function Card({ children }: Props) {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="flex-cols m-auto flex h-3/4 w-3/4 gap-5 rounded-3xl bg-white p-5 shadow-lg">
+      <div className="flex-cols m-auto flex  w-3/4 max-w-6xl gap-5 rounded-3xl bg-white p-5 shadow-lg">
         <div
           className="hidden w-80 space-y-5 rounded-xl bg-cover p-10 text-white lg:block"
           style={{ backgroundImage: "url(/bg-sidebar-desktop.svg)" }}
@@ -19,7 +19,7 @@ export default function Card({ children }: Props) {
             return <Step title={title} currentStep={number + 1} key={number} />;
           })}
         </div>
-        <div className="">{children}</div>
+        <div className="w-full space-y-5 p-5 md:p-14">{children}</div>
       </div>
     </div>
   );
--- src/components/steps/YourInfo.tsx
+++ src/components/steps/YourInfo.tsx
@@ -1,3 +1,44 @@
 export default function YourInfo() {
-  return <div>YourInfo</div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-900 lg:text-5xl">
+        Personal info
+      </h1>
+      <p className="text-gray-500">
+        Please provide your name, email address, and phone number
+      </p>
+
+      <form className="flex flex-col space-y-2">
+        <label className="text-blue-900 ">Name</label>
+        <input
+          className="rounded-lg border-2 p-3 focus:outline-blue-900/50"
+          type="text"
+          placeholder="e.g Stephen King"
+        />
+
+        <label className="pt-3 text-blue-900">Email address</label>
+        <input
+          className="rounded-lg border-2 p-3 focus:outline-blue-900/50"
+          type="email"
+          placeholder="e.g Stephen King"
+        />
+
+        <label className="pt-3 text-blue-900">Phone number</label>
+        <input
+          className="rounded-lg border-2 p-3 focus:outline-blue-900/50"
+          type="tel"
+          placeholder="e.g Stephen King"
+        />
+
+        <br />
+
+        <button
+          className="ml-auto w-fit rounded-lg bg-blue-900 px-2 py-3 text-white focus:outline-blue-400"
+          type="submit"
+        >
+          Next step
+        </button>
+      </form>
+    </>
+  );
 }

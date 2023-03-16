2b941f4b6913102f03f668b36c734e972d250235
Rubén Chiquin <randreu28@gmail.com>
2023-03-16 19:52:23 +0100

    Creates and implements a Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,26 @@
-export default function Card() {
-  return <div>Card</div>;
+import { ReactElement } from "react";
+import Step from "./Step";
+
+interface Props {
+  children: ReactElement;
+}
+
+export default function Card({ children }: Props) {
+  const steps = ["Your info", "Select plan", "Add-ons", "Summary"];
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex-cols m-auto flex h-3/4 w-3/4 gap-5 rounded-3xl bg-white p-5 shadow-lg">
+        <div
+          className="hidden w-80 space-y-5 rounded-xl bg-cover p-10 text-white lg:block"
+          style={{ backgroundImage: "url(/bg-sidebar-desktop.svg)" }}
+        >
+          {steps.map((title, number) => {
+            return <Step title={title} currentStep={number + 1} key={number} />;
+          })}
+        </div>
+        <div className="">{children}</div>
+      </div>
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Card from "./components/Card";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Card>
+      <App />
+    </Card>
   </React.StrictMode>
 );
